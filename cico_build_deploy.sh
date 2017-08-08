9f04a3cd21cdbffab02e244f9890d816fdf0daf8
Václav Pavlín <vaclav.pavlin@gmail.com>
2017-08-08 09:53:48 +0200

    Use push.registry.devshift.net

--- cico_build_deploy.sh
+++ cico_build_deploy.sh
@@ -7,7 +7,7 @@ set -x
 set -e
 
 # Export needed vars
-for var in GIT_COMMIT; do
+for var in GIT_COMMIT DEVSHIFT_USERNAME DEVSHIFT_PASSWORD; do
   export $(grep ${var} jenkins-env | xargs)
 done
 export BUILD_TIMESTAMP=`date -u +%Y-%m-%dT%H:%M:%S`+00:00
@@ -23,11 +23,14 @@ service docker start
 # Build the app
 
 IMAGE="zabbix-status-api"
-REPOSITORY="registry.devshift.net/openshiftio"
+REGISTRY="push.registry.devshift.net"
+REPOSITORY="${REGISTRY}/openshiftio"
 
 docker build -t ${IMAGE} -f Dockerfile .
 
 TAG=$(echo $GIT_COMMIT | cut -c1-6)
+
+docker login -u ${DEVSHIFT_USERNAME} -p ${DEVSHIFT_PASSWORD} ${REGISTRY}
 
 docker tag ${IMAGE} ${REPOSITORY}/${IMAGE}:$TAG && \
 docker push ${REPOSITORY}/${IMAGE}:$TAG && \
